fbc3ef5c1d5e26dafc0553e717d9235fb0f44c53
ɐʇɯon3ɹ <contact.atmoner@gmail.com>
2021-09-01 17:12:13 +0000

    Create bitcanna-ws.js

---
+++ bitcanna-ws.js
@@ -0,0 +1,27 @@
+// Call webSocket from tendermint
+// https://docs.tendermint.com/master/rpc/
+// Coded by @atmon3r for Bitcanna 💚
+
+import WebSocket from 'ws';
+
+const ws = new WebSocket('ws://seed1.bitcanna.io:26657/websocket');
+
+ws.on('open', function open() {
+	console.log('Connected on Bitcanna blockchain form WebSocket');
+	ws.send(JSON.stringify({
+		"method":"subscribe",
+		"params": ["tm.event='NewBlock'"],
+		"id":"1",
+		"jsonrpc":"2.0"
+	}));
+});
+
+ws.on('close', function close() {
+	console.log('disconnected');
+});
+ 
+ws.on('message', function incoming(data) {
+	var finalData = JSON.parse(data.toString('utf-8'));
+	if (finalData.result.data)
+		console.log(finalData.result);
+}); 
