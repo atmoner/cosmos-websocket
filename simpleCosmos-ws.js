43bd92eb7b7dbfc6216598ae119aeafd5b401069
ɐʇɯon3ɹ <contact.atmoner@gmail.com>
2022-01-14 13:59:50 +0100

    Update simpleCosmos-ws.js

--- simpleCosmos-ws.js
+++ simpleCosmos-ws.js
@@ -7,7 +7,7 @@ import WebSocket from 'ws';
 const ws = new WebSocket('ws://seed1.bitcanna.io:26657/websocket');
 
 ws.on('open', function open() {
-	console.log('Connected on Bitcanna blockchain form WebSocket');
+	console.log('Connected on Bitcanna testnet-1 blockchain form WebSocket');
 	ws.send(JSON.stringify({
 		"method":"subscribe",
 		"params": ["tm.event='NewBlock'"],
